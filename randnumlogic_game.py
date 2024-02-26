a79af320a4ad626a5da9766943297023f459923f
Elnura <elya.ayase@gmail.com>
2024-02-27 00:35:50 +0400

    Update randnumlogic_game.py

--- randnumlogic_game.py
+++ randnumlogic_game.py
@@ -3,19 +3,21 @@ import random
 def generate_random_number():
 return random.randint(1, 100)
 
-random_number = generate_random_number()
 
-def guess_number():
- guessed =  False 
+def guess_number():                                                # func for guessing logic
+ random_number =generate_random_number()                           #generating rand num
+ guessed =  False                                                  # variable is boolean = false for not guessed nums 
 
-while not guessed:
- guess =  int(input(" Let's guess the number from (1,100)..")) 
- if guess < random_number:
-  print ("Lower than 0, try once again!")
-  elif guess > random_number: 
-  print ("Higher than 100, give it another try!")\
- else:
-  print ("Wow! You guessed it right")
-   guessed = True
+while not guessed:                                                 # start a while loop until guessed becomes"true"
+ guess =  int(input(" Let's guess the number from (1,100).."))     # asking user for an number and converting to int
+ if guess < random_number:                                         # checking if num is <
+  print ("Lower than 0, try once again!")                          # informing 
+  elif guess > random_number:                                      # another check
+  print ("Higher than 100, give it another try!")                  # informing 2
+ else:                                                             # if correct 
+  print ("Wow! You guessed it right")                              # informing 3
+   guessed = True                                                  # setting guess to true to e xit the while  loop 
 
-guess_number ()
+
+if __name__ == "__main__"                                          # pyhton check if script is running directly not from the module  , if yes then the name is set to the name of module 
+guess_number ()                                                    # call the guess_number func to start the game
