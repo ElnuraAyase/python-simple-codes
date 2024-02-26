eedcbea0732756852dce3aea992a828e03aec062
Elnura <elya.ayase@gmail.com>
2024-02-27 00:23:29 +0400

    Update randnumlogic_game.py

--- randnumlogic_game.py
+++ randnumlogic_game.py
@@ -5,7 +5,16 @@ return random.randint(1, 100)
 
 random_number = generate_random_number()
 
-def guess number():
+def guess_number():
  guessed =  False 
 
+while not guessed:
+ guess =  int(input(" Let's guess the number from (1,100)..")) 
+ if guess < random_number:
+  print ("Lower than 0, try once again!")
+  elif guess > random_number: 
+  print ("Higher than 100, give it another try!")
+else:
+print ("Wow! You guessed it right")
+guessed = True
 
