b25d814702dd40794f88f5df5d3c69df5c5f7d54
Elnura <elya.ayase@gmail.com>
2024-02-27 00:08:14 +0400

    Create 3stepcode.py

---
+++ 3stepcode.py
@@ -0,0 +1,4 @@
+import random # import random helpt to generate random nums
+def generate_random_number():  # def = defined function for random nums
+  return random(1,100) # randit function is used from random module to generate nums from 1 - 100  
+rundom_numbe r= generate_random_number(); # calling the previous function generate_random_number for storin g in the variable random_number  
