963641ee1054514c32cf7b86c765bb0d85498581
Elnura <elya.ayase@gmail.com>
2024-02-29 23:55:10 +0400

    Create simple_calc.py

---
+++ simple_calc.py
@@ -0,0 +1,14 @@
+# ask user to choose the operation
+
+print ("Select operation: ");
+print ("1. Addition");
+print ("2. Substraction")
+print ("3. Division")
+print("4. Multiplication")
+
+choice= input (" Enter your choice : ( 1 / 2 / 3 / 4 ): ")
+
+#asking user to inpt 2 nums 
+
+num1 = float(input("enter any number")) 
+num2 float(input("enter another number"))
