e8cce57d76cfe776bc7f765ad54bce9e2f37d029
Elnura <elya.ayase@gmail.com>
2024-02-29 23:59:11 +0400

    Update simple_calc.py

--- simple_calc.py
+++ simple_calc.py
@@ -1,14 +1,26 @@
-# ask user to choose the operation
+# step 1: ask user to choose the operation
 
 print ("Select operation: ");
 print ("1. Addition");
 print ("2. Substraction")
-print ("3. Division")
-print("4. Multiplication")
+print ("3. Multiplication")
+print("4. Division"")
 
 choice= input (" Enter your choice : ( 1 / 2 / 3 / 4 ): ")
 
-#asking user to inpt 2 nums 
+# step 2: asking user to inpt 2 nums 
 
 num1 = float(input("enter any number")) 
 num2 float(input("enter another number"))
+
+#step 3 perfoming the answer
+
+if choice == '1' :
+  result = num1 + num2 
+  operation = "addition"
+elif choice == '2'
+result = num1 - num2 
+operation = "substraction"
+elif choice == '3'
+result = num1 * num2 
+else choice == '4'
