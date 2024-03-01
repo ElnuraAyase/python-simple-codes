f075c68229f69b301d421fb7a9e6c187a7666731
Elnura <elya.ayase@gmail.com>
2024-03-01 23:52:55 +0400

    Update simple_english_game.py

--- simple_english_game.py
+++ simple_english_game.py
@@ -1,2 +1,14 @@
 def display_word(word, guessed_letters):
-display
+  display = ""
+  for letter in word:
+    if letter in guessed_letters:
+      display =+ letter
+      else :
+      display += "-"
+    return display
+  def main ():
+    print = ("Welcome to the "Simple Guess" English game! ")
+    word_to_guess = input("Enter the word you want to guess later").lower()
+    guessed_letters = []
+    attempts = 5 
+  
