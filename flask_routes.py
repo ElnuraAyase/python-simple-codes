b268d9ae79730d7b6e3c263ac2f9bbe079a78e35
Elnura <elya.ayase@gmail.com>
2024-03-16 22:01:13 +0400

    Update flask_routes.py
    added comms

--- flask_routes.py
+++ flask_routes.py
@@ -1,24 +1,26 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template  # Import the Flask class and render_template fUNCTION
 
-app = Flask(__name__)
+app = Flask(__name__)   # Create a Flask application instance
 
-@app.route('/')
+# Define a route for the hmpage
+@app.route('/')  
 def index():
-    return 'Welcome to the homepage!'
-
+    return 'Welcome to the homepage!'   # simple string 
+# ROUTE PAGE FOR ABOUT
 @app.route('/about')
 def about():
     return 'This is the about page.'
-
+# ROUTE FOR CONTACT
 @app.route('/contact')
 def contact():
     return 'Contact us at example@example.com.'
-
+# Define a route that takes a parameter and greets the user with that name
+@app.route 
 @app.route('/hello/<name>')
 def hello(name):
     return f'Hello, {name}!'
-
+# Run the Flask application if this script is executed
 if __name__ == '__main__':
     app.run(debug=True)
 
