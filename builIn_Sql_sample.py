0bdaf9dae5daa930f5c1541f46d752d66ac2b99d
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-05-02 13:32:56 -0900

    Update builIn_Sql_sample.py

--- builIn_Sql_sample.py
+++ builIn_Sql_sample.py
@@ -22,10 +22,27 @@ conn.commit()
 # data retrieve 
 
 cursor.execute( "SELECT * FROM users" ) 
+#additional 1 
+rows = cursor.fetchall()
 
 # print 
-for in rows:
-print (row) 
+for row in rows:
+   print (row) 
+#addition 2 
+#updating table
 
+cursor.execute( " UPDATE users SET age = ? WHERE name = ? ", (29, Kim) )
+conn.commit()
+print ( "\nUPDATEd Kim's age.")
+
+# retriving after updates 
+cursor.execute( "SELECT * FROM USERS")
+updated_rows = cursor.fetchall()
+
+#print updated info 
+print ("\nData in the table after updating: ")
+for row in updated_rows:
+  print(row) 
+  
 #closing connection 
 conn.close()
