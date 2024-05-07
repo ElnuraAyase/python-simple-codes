b94f9e509d490411679259afcb89d6da810de505
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-05-08 02:11:58 +0400

    Update fibonacci_nums.py

--- fibonacci_nums.py
+++ fibonacci_nums.py
@@ -3,4 +3,8 @@ def fibonacci(n):
         return n
     else:
         return fibonacci(n-1) + fibonacci (n-2)
+        def fibonacci(n):
+
+result =  [fibonacci(i) for i in range(10)]
+print(result)  
 
