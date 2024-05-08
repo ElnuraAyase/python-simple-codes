1cbbac006c6f349cce33f9c464c8d60be8d05751
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-05-08 13:41:51 +0400

    Update user_interacction_samples.py

--- user_interacction_samples.py
+++ user_interacction_samples.py
@@ -1,8 +1,22 @@
-#any input form the user to be printed
+#1 any input form the user to be printed
 print( "enter todo:")
 user_outp = input()
 print(user_outp)
-#same but different way 
+# 2 same but different way 
 prompt =  "enter todo: "
 user1_txt = input(prompt) 
 print(user1_txt) 
+
+# 3make multile todos
+user_prompt = "enter todo: "
+#do not put number before variable
+todo1= input(user_prompt) 
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+#creating list for mult variables
+todos = [todo1, todo2, todo3]
+print(todos) 
+
+# add info about type of value associated with variable (string,int or list ,etc.) 
+print(type(todos) # output will be <class 'list>
+print (type(user_prompt) # output will be < class  'str'>
